2984237303edebd3a69c3c6346ecc17e22d8b148
Maurizio Pillitu <maoo@symphony.foundation>
2016-07-22 22:01:04 +0200

    changed require metadata rule strategy; improved readme, added pom.xml elements

--- README.md
+++ README.md
@@ -2,13 +2,41 @@
 A collection of custom rules used to enforce Maven projects hosted by the Symphony Software Foundation.
 These rules are used by the [Symphony Foundation Parent Pom](http://github.com/symphonyoss/ssf-parent-pom)
 
+Usage:
+```
+<plugin>
+    <groupId>org.apache.maven.plugins</groupId>
+    <artifactId>maven-enforcer-plugin</artifactId>
+    <configuration>
+      <rules>
+        <myCustomRule implementation="org.symphonyoss.maven.enforcer.RequireGroupIdRule">
+          <groupIdPrefix>org.symphonyoss</groupIdPrefix>
+        </myCustomRule>
+        <myCustomRule implementation="org.symphonyoss.maven.enforcer.RequireFileInArtifactRule">
+          <fileName>NOTICE</fileName>
+          <match>The Symphony Software Foundation (http://symphony.foundation)</match>
+        </myCustomRule>
+        <myCustomRule implementation="org.symphonyoss.maven.enforcer.RequireFileInArtifactRule">
+          <fileName>LICENSE</fileName>
+          <match>Copyright 2016 The Symphony Software Foundation</match>
+        </myCustomRule>
+        <myCustomRule implementation="org.symphonyoss.maven.enforcer.RequirePomMetadataRule"/>
+      </rules>
+    </configuration>
+</plugin>
+
+```
+
 ## RequireFileInArtifactRule
 Fails if a given `<filename>` doesn't exist in the (JAR/WAR/ZIP) artifact built by Maven  (`${project.build.directory}/${project.artifactId}-${project.version}.${project.packaging}`); you can optionally specify a `<match>` string to be matched within the file content.
 
 ## RequireGroupIdRule
 Fails if the groupId of the current project doesn't match (or start with) `<groupIdPrefix>`
 
-## RequireMetadataRule
-Fails if a given pom.xml metadata (`<metadataName>`) is not defined; you can optionally specify an `<expectedStringValue>` to validate the expected value 
-
-
+## RequirePomMetadataRule
+Fails if the Maven Central requirements for publishing are not respected by the root pom.xml of the current project; the elements checked are:
+- `<url>`
+- `<description>`
+- `<scm>`
+- `<developers>`
+- `<licenses>`
---
+++ src/main/java/org/symphonyoss/maven/enforcer/RequirePomMetadataRule.java
@@ -0,0 +1,106 @@
+/*
+ *
+ * Copyright 2016 The Symphony Software Foundation
+ *
+ * Licensed to The Symphony Software Foundation (SSF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.symphonyoss.maven.enforcer;
+
+import org.apache.maven.enforcer.rule.api.EnforcerRule;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleException;
+import org.apache.maven.enforcer.rule.api.EnforcerRuleHelper;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.plugin.logging.Log;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:maoo@symphony.foundation">Maurizio Pillitu</a>
+ * This Rule fails if either <scm>, <developers> or <licenses> is not set in the root pom.xml
+ *
+ */
+public class RequirePomMetadataRule
+    implements EnforcerRule
+{
+
+    private Log log;
+    
+    public void execute( EnforcerRuleHelper helper )
+        throws EnforcerRuleException
+    {
+        log = helper.getLog();
+
+        try
+        {
+            String error = validatePom();
+            if (error != null) {
+                throw new EnforcerRuleException("RequirePomMetadataRule is failing with the following error: " + error);
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot find root parent pom.xml", e );
+        }
+    }
+
+    private Model pomToModel(String pathToPom) throws IOException, XmlPullParserException {
+        BufferedReader in = new BufferedReader(new FileReader(pathToPom));
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(in);
+        return model;
+    }
+
+    private String validatePom() throws IOException, XmlPullParserException {
+        Model pomModel = pomToModel("./pom.xml");
+        if (pomModel.getDescription() == null || pomModel.getDescription().isEmpty()) {
+            return "pom.xml is missing <description> element  or it's empty";
+        }
+        if (pomModel.getUrl() == null || pomModel.getUrl().isEmpty()) {
+            return "pom.xml is missing <url> element or it's empty";
+        }
+
+        if (pomModel.getScm() == null) {
+            return "pom.xml is missing <scm> element";
+        }
+        if (pomModel.getLicenses().size() == 0) {
+            return "pom.xml is missing <licenses> element";
+        }
+        if (pomModel.getDevelopers().size() == 0) {
+            return "pom.xml is missing <developers> element";
+        }
+        return null;
+    }
+
+    public boolean isCacheable() {
+        return false;
+    }
+
+    // Not used if cachable=false
+    public String getCacheId() {
+        return ""+this.getClass().hashCode();
+    }
+
+    // Not used if cachable=false
+    public boolean isResultValid( EnforcerRule arg0 ) {
+        return false;
+    }
+}
